4a84c60cdf1ca2d849ae8d40714286b726f8c1ed
Emmanue Imbernon <Cowprod@users.noreply.github.com>
2026-09-17 10:28:32 +0200

    Add SAF JavaScript bridge

---
+++ tests/plugin-lab/local-plugins/cordova-plugin-multicam-saf/www/MultiCamSaf.js
@@ -0,0 +1,4 @@
+var exec = require('cordova/exec');
+exports.chooseDirectory = function(success, error) { exec(success, error, 'MultiCamSaf', 'chooseDirectory', []); };
+exports.testWrite = function(uri, success, error) { exec(success, error, 'MultiCamSaf', 'testWrite', [uri]); };
+exports.listPersisted = function(success, error) { exec(success, error, 'MultiCamSaf', 'listPersisted', []); };
